dc45315a2449235ddc1bc2a681a527af144f18d9
Jan <janekrembikowski@gmail.com>
2023-03-27 00:10:00 +0200

    code refactor #2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import NavBar from "./components/NavBar/NavBar";
 import { useState } from "react";
 import BookList from "./components/BookList/BookList";
-import usersJson from './models/users.json';
 import data from "./models/books.json";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/styles/global-styles.css';
 
 function App() {
-
     const [books, setBooks] = useState([]);
 
     const handleSearchSubmit = (query) => {
-            const result = data.filter((item) => {
-                            return query === '' ? item :
-                            item.title.toString().toLowerCase().includes(query.toLowerCase())
-                            })
-            setBooks(result);
+        const result = data.filter((item) => {
+            return query === '' ? item :
+                item.title.toString().toLowerCase().includes(query.toLowerCase())
+        })
+        setBooks(result);
     }
 
 
     return (
-            <>
-                <NavBar onSubmit={handleSearchSubmit}/>
-                <BookList books={books}/>
-            </>
-        )
+        <>
+            <NavBar onSubmit={handleSearchSubmit} />
+            <BookList books={books}/>
+        </>
+    )
 };
 
 export default App;
