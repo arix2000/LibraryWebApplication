cd13e1ea200e31e66b0f485f553b1c11675f4b3e
Jan <janekrembikowski@gmail.com>
2023-03-28 03:53:29 +0200

    add modal component

--- src/components/BookShow/BookShow.js
+++ src/components/BookShow/BookShow.js
@@ -5,33 +5,45 @@ import Card from 'react-bootstrap/Card';
 import { Col, Row } from 'react-bootstrap';
 import { Rating } from '@mui/material';
 import { BsFillCartPlusFill } from "react-icons/bs";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { useState } from "react";
+import BookDetail from "../BookList/BookDetail";
 
 export default function BookShow({ book }) {
-    
+
+    const [detailShow, setDetailShow] = useState(false);
+
     return(
-        <Card
-        className="card text-center pb-0 pr-0 mt-5 card-radius label-color"
-        text="light">
-                <Row g={5}>
-                    <Col md={4} my={0} pt={5}>
-                        <Card.Img
-                        className="card-img"
-                        src={book.thumbnail} 
-                        alt={book.title + " Cover"}
-                        />
-                    </Col>
-                    <Col md={8}>
-                        <Card.Body className="p-0">
-                            <Card.Title className="mt-2 mb-3">{'"' + book.title + '"'}</Card.Title>
-                            <Card.Title className="author mb-1">{book.authors.replaceAll(';', ", ")}</Card.Title>
-                            <Button variant="primary mt-3 mb-3 px-3"><BsFillCartPlusFill style={{width: 24, height: 24}}/></Button>
-                        </Card.Body>
-                        <Card.Footer className="text-center">
-                            <h4>{book.average_rating}</h4>
-                            <Rating name="half-rating-read" defaultValue={book.average_rating} precision={0.1} readOnly/>
-                        </Card.Footer>
-                    </Col>
-                </Row>
-        </Card>
+        <>
+            <Card
+            className="card text-center mt-5 card-radius label-color"
+            text="light">
+                    <Row g={5}>
+                        <Col md={4} my={0} pt={5}>
+                            <Card.Img
+                            className="card-img"
+                            src={book.thumbnail} 
+                            alt={book.title + " Cover"}
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <Card.Body>
+                                <Card.Title className="mb-2">{'"' + book.title + '"'}</Card.Title>
+                                <Card.Title className="author mt-2 font-italic">{book.authors.replaceAll(';', ", ")}</Card.Title>
+                                <Card.Title><Button variant="success mt-2 mb-2" px={3}><BsFillCartPlusFill style={{width: 24, height: 24}}/></Button></Card.Title>
+                                <Card.Title><Rating name="half-rating-read" defaultValue={book.average_rating} precision={0.1} readOnly/></Card.Title>
+                                <Button variant="primary detail-button text-center mt-4"
+                                        onClick={() => setDetailShow(true)}>
+                                            <FiMoreHorizontal style={{width: 40, height: 16}}/>
+                                    </Button>
+                                <BookDetail
+                                show={detailShow}
+                                onHide={() => setDetailShow(false)}
+                                book={book}/>
+                            </Card.Body>
+                        </Col>
+                    </Row>
+            </Card>
+        </>
         )
 };
