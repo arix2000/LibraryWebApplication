8acb0a10da8a4a5d486fd84df483db2cc73be9f8
Jan <janekrembikowski@gmail.com>
2023-04-03 13:33:03 +0200

    fix styling and add search condition

--- src/components/BookSearch/BookDetail/BookDetail.js
+++ src/components/BookSearch/BookDetail/BookDetail.js
@@ -3,31 +3,35 @@ import { Modal, Button } from "react-bootstrap";
 import BorrowButton from "../common/BorrowButton";
 
 export default function BookDetail({ show, onHide, book }) {
-
-    return (
-        <Modal
-            show={show}
-            onHide={onHide}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header className="custom-header label-color upper-radius">
+        <Modal.Title
+          id="contained-modal-title-vcenter"
+          className="text-center w-100 pb-2 fade-in"
         >
-            <Modal.Header className="custom-header label-color">
-              <Modal.Title id="contained-modal-title-vcenter"
-              className="text-center w-100 pb-2 fade-in"
-              style={{ "--anim-time": "2s" }}>
-                  <h1 className="font-weight-bold">{'"' + book.title + '"'}</h1>
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="custom-body px-5">
-              <h4 className="font-italic font-weight-bold my-3 fade-in" style={{ "--anim-time": "3s" }}>{book.authors.replaceAll(';', ", ")}</h4>
-              <h5 className="my-4 font-weight-bold fade-in" style={{ "--anim-time": "4s" }}>{book.categories}</h5>
-              <p className="mt-4 fade-in" style={{ "--anim-time": "5s", "text-align": "justify" }}>{book.description}</p>
-            </Modal.Body>
-            <Modal.Footer className="custom-footer px-5 py-2 label-color" style={{disply:'flex', justifyContent:'space-between'}}>
-              <BorrowButton width={60} height={30}/>
-              <Button className="ml-3 button-radius" onClick={onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-      );
+          <h1 className="font-weight-bold">{'"' + book.title + '"'}</h1>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="custom-body px-5">
+        <h4 className="font-italic font-weight-semibold my-3 fade-in">
+          {book.authors.replaceAll(";", ", ")}
+        </h4>
+        <h5 className="my-4 gray-text fade-in">{book.categories}</h5>
+        <p className="mt-4 fade-in">{book.description}</p>
+      </Modal.Body>
+      <Modal.Footer className="custom-footer px-3 py-2 label-color bottom-radius">
+        <Button className="mr-3 button-radius" onClick={onHide}>
+          Close
+        </Button>
+        <BorrowButton width={35} height={21} useIcon={false}/>
+      </Modal.Footer>
+    </Modal>
+  );
 }
--- src/components/BookSearch/common/BorrowButton.js
+++ src/components/BookSearch/common/BorrowButton.js
@@ -2,18 +2,22 @@ import { BsFillCartPlusFill } from "react-icons/bs";
 import { Button } from "react-bootstrap";
 import "./common-styles/borrow-button.css";
 
-export default function BorrowButton({ width, height }) {
+export default function BorrowButton({ width, height, useIcon }) {
+  const buttonHandler = (e) => {
+    console.log("borrowed");
+    e.stopPropagation();
+  };
 
-    const buttonHandler = (e) => {
-        console.log("borrowed");
-        e.stopPropagation();
-      };
-      
-    return(
-        <Button variant="success" className="border-radius">
-            <BsFillCartPlusFill 
-            style={{width: width, height: height}}
-            onClick={buttonHandler}/>
-        </Button>
-    );
+  return (
+    <Button variant="success" className="button-radius">
+      {useIcon ? (
+        <BsFillCartPlusFill
+          style={{ width: width, height: height }}
+          onClick={buttonHandler}
+        />
+      ) : (
+        <span onClick={buttonHandler}>Borrow</span>
+      )}
+    </Button>
+  );
 }
