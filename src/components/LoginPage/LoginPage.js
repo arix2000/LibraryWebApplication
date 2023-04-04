9506adf0d7d88afc3b55afe82a6b8e029517b39d
maciekkielducki <maciek.kielduckiii@icloud.com>
2023-04-04 23:28:43 +0200

    fixed color for label

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -5,39 +5,31 @@ import UserManager from "../admin_panel/utils/UserManager";
 export default function LoginPage() {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
   const userManager = new UserManager();
   
   const handleSubmit = (event) => {
     event.preventDefault();
-    const users = userManager.getUsers();
-    const user = users.find((u) => u.login === login && u.password === password);
-    if (user) {
-      setMessage('Zalogowano!');
-    } else {
-      setMessage('Nieprawidłowy email lub hasło');
-    }
+    const isLoginSuccess = userManager.getUsers().find((u) => u.login === login && u.password === password);
   };
 
   return (
     <div className="background-color">
       <div className="background-color">
         <Container>
-          <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Row className="vh-100 d-flex justify-content-center align-items-center" >
             <Col md={16} lg={6} xs={24}>
-              <Card className="shadow label-color">
+              <Card className="shadow label-color" style={{borderRadius: '2rem'}}>
                 <Card.Body>
                   <div className="mb-3 mt-md-4">
                     <h2 className="text-center text-light">Fish library</h2>
-                    {message && <p className={message === 'Zalogowano!' ? 'text-success' : 'text-danger'}>{message}</p>}
                     <div className="mb-3">
                       <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Group className="mb-3">
                           <Form.Label className="text-center text-light">
-                            Email adress
+                            Login
                           </Form.Label>
                           <Form.Control
-                            className="bg-secondary border border-dark text-light"
+                            className="default-text-field"
                             type="login"
                             placeholder="Enter login"
                             value={login}
@@ -47,21 +39,14 @@ export default function LoginPage() {
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                           <Form.Label className="text-light">Password</Form.Label>
                           <Form.Control
-                            className="bg-secondary border border-dark text-light"
+                            className="default-text-field"
                             type="password"
                             placeholder="Password"
                             value={password}
                             onChange={(event) => setPassword(event.target.value)}
                           />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                          <p className="small text-center">
-                            <a className="text-light" href="#!">
-                              Forgot password?
-                            </a>
-                          </p>
-                        </Form.Group>
-                        <div className="d-grid button-color">
+                        <div className="d-grid button-color" variant="secondary">
                           <Button type="submit">Login</Button>
                         </div>
                       </Form>
