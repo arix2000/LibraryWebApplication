f8097fa155392ac3414e6c9a7614693f8df8bc13
Jan <janekrembikowski@gmail.com>
2023-04-16 17:45:36 +0200

    make carousel reusable

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,19 +1,45 @@
 import NavBar from "../UiCommon/NavBar";
-import RecommCarousel from "./RecommCarousel";
+import HomePageCarousel from "./HomePageCarousel";
 import styles from "../styles/homePage/homePage.module.css";
 import books from "../../models/books.json";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function HomePage() {
+  const recommBooks = [books[2676], books[65], books[3955]];
+  const bestBooks = [books[12], books[19]];
+  const borrowedBooks = [books[126], books[718], books[351], books[913], books[83]];
+
   return (
     <>
       <NavBar showSearchBar={false} />
-      <div className={styles.outer}>
-        <div className={styles.recommBadge}>
-          <h4>Recommended</h4>
-          <RecommCarousel books={books}/>
-        </div>
-      </div>
-      
+      <Container>
+        <Row>
+          <Col>
+            <div className={styles.outer}>
+              <div className={styles.carouselBadge}>
+                <h4>Recommended</h4>
+                <HomePageCarousel books={recommBooks} />
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.outer}>
+              <div className={styles.carouselBadge}>
+                <h4>Bestsellers</h4>
+                <HomePageCarousel books={bestBooks} />
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.outer}>
+              <div className={styles.carouselBadge}>
+                <h4>Borrowed</h4>
+                <HomePageCarousel books={borrowedBooks} />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
---
+++ src/components/HomePage/HomePageCarousel.js
@@ -0,0 +1,43 @@
+import { Carousel } from "react-bootstrap";
+import styles from "../styles/homePage/homePage.module.css";
+import BookDetailModal from "../BookSearch/BookDetail/BookDetailModal";
+import { useState } from "react";
+
+export default function HomePageCarousel({ books }) {
+  const [detailShow, setDetailShow] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleImgClick = (book) => {
+    setSelectedBook(book);
+    setDetailShow(true);
+  };
+
+  return (
+    <>
+      <Carousel
+        className={styles.carousel}
+        nextLabel=""
+        variant="dark"
+        prevLabel=""
+      >
+        {books.map((book) => (
+          <Carousel.Item key={book.isbn13}>
+            <img
+              className={styles.carouselImg}
+              src={book.thumbnail}
+              alt={book.title}
+              onClick={() => handleImgClick(book)}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      {selectedBook && (
+        <BookDetailModal
+          show={detailShow}
+          onHide={() => setDetailShow(false)}
+          book={selectedBook}
+        />
+      )}
+    </>
+  );
+}
