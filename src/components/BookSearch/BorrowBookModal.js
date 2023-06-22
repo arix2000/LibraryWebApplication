fcf6042653543dcdbe5e34b6799ef4ef29a0a1db
Jan <janekrembikowski@gmail.com>
2023-06-22 04:21:18 +0200

    add users filtering

--- src/components/BookSearch/BorrowBookModal.js
+++ src/components/BookSearch/BorrowBookModal.js
@@ -1,16 +1,58 @@
-import { Modal, Button, Table } from "react-bootstrap";
+import { Modal, Button, Table, Form } from "react-bootstrap";
 import styles from "../styles/bookItem.module.css";
 import UserManager from "../AdminPanel/utils/UserManager";
 import { useState, useEffect, useCallback } from "react";
+
+function UserRow({ users, query, selectedUserId, handleClick }) {
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(query.toLowerCase()) ||
+      user.surname.toLowerCase().includes(query.toLowerCase()) ||
+      user.login.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return (
+    <>
+      {filteredUsers.map((user) => {
+        console.log(user);
+        const isSelected = user.id === selectedUserId;
+        const rowClass = isSelected ? styles.selectedRow : styles.usersTableRow;
+        return (
+          <tr className={rowClass} key={user.id} onClick={() => handleClick(user.id)}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.surname}</td>
+            <td>{user.login}</td>
+            <td>{user.reserved_books.length}</td>
+            <td>{user.borrowed_books.length}</td>
+          </tr>
+        );
+      })}
+    </>
+  );
+}
 
 export default function BorrowBookModal({ show, onHide, book }) {
   const userManager = new UserManager();
 
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleClick = (userId) => {
+    setSelectedUserId(userId);
+    setIsChecked(true);
+  };
+
+  const handleQueryChange = (e) => {
+    const inputValue = e.target.value;
+    setQuery(inputValue);
+  };
 
   const assignUsersToVar = useCallback(() => {
     setUsers(userManager.getUsers());
-  }, [users]);
+  }, []);
 
   useEffect(() => {
     assignUsersToVar();
@@ -18,43 +60,55 @@ export default function BorrowBookModal({ show, onHide, book }) {
 
   return (
     <Modal
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
       show={show}
       onHide={onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header className={`text-light label-color ${styles.borrowBookModalHeader}`}>
+      <Modal.Header
+        className={`text-light label-color ${styles.borrowBookModalHeader}`}
+      >
         <Modal.Title
           id="contained-modal-title-vcenter"
           className="text-center w-100 pb-2 fade-in"
         >
-          <h1 className="font-weight-bold">Choose user from list below</h1>
+          <h1 className="font-weight-bold">{'"' + book.title + '"'}</h1>
+          <h5>Select the user to whom you wish to lend the book.</h5>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className={`px-5 text-light ${styles.borrowBookModalBody}`}>
+        <Form>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className={styles.userSearchForm}
+            aria-label="Search"
+            onChange={handleQueryChange}
+            value={query}
+          />
+        </Form>
         <Table responsive className={styles.usersTable}>
           <thead>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Login</th>
-            <th>Reserved</th>
-            <th>Borrowed</th>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Login</th>
+              <th>Reserved</th>
+              <th>Borrowed</th>
+            </tr>
           </thead>
           <tbody>
-            {users.map((user) => {
-              console.log(user);
-              return(
-                 <tr className={styles.usersTableRow}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.surname}</td>
-                    <td>{user.login}</td>
-                    <td>{user.reserved_books.length}</td>
-                    <td>{user.borrowed_books.length}</td>
-                </tr>);
-            })}
+            <UserRow
+              users={users}
+              query={query}
+              selectedUserId={selectedUserId}
+              handleClick={handleClick}
+            />
           </tbody>
         </Table>
       </Modal.Body>
@@ -63,6 +117,14 @@ export default function BorrowBookModal({ show, onHide, book }) {
       >
         <Button className="button-radius" variant="secondary" onClick={onHide}>
           Close
+        </Button>
+        <Button
+          className="button-radius"
+          variant="success"
+          onClick={onHide}
+          disabled={!isChecked}
+        >
+          Hire a book
         </Button>
       </Modal.Footer>
     </Modal>
